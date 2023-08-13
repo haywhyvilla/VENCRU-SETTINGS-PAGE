90d9f90181e7f7ad1e5c53044e969dfe9f3c042d
haywhyvilla <olatunjiayobami575@gmail.com>
2023-08-13 11:31:39 +0100

    new commit

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,5 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
     title: "VENCRU-SETTINGS-PAGE",
@@ -36,7 +34,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body>{children}</body>
         </html>
     );
 }
